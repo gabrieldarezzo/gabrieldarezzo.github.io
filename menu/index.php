66183d0879bd4b3e51b627d4303e045da7628bf4
Gabriel Darezzo <gabrieldarezzo@yahoo.com>
2016-05-01 11:28:43 -0300

    fav

---
+++ menu/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link href="custom.css" rel="stylesheet"/>
+	<link rel="icon" href="favicon.png">
+</head>
+<body>
+
+<div id="wrapper">
+	<div id="menu">
+			aaa
+		</div>
+	<div id="conteudo">
+		
+		a
+
+	</div>
+</div>
+
+<div id="rodape">
+	Todos direitos liberados haha
+</div>
+	
+	
+	
+</body>
+</html>
+
+
+<a href="?id...&pagina'.PAGINA_ATUAL.'">Editar</a>
